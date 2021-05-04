9c389d6f54080ef39219b9a2d6b31b8b76b6f618
AnthonyMarc23 <anthony@anthonymarc.me>
2021-05-04 19:05:58 -0400

    updates to functions
    fixed memoized Functions, broke adding tasks. will fix in next build

--- src/src/App.js
+++ src/src/App.js
@@ -5,28 +5,48 @@ import { useState } from 'react';
 
 const App = () => {
   const [items, setItems] = useState([]);
+  const [filter, setFilter] = useState(null);
 
   const inputEntry = (addItem) => {
     //grab input from input field and save to items Array
-    setItems([...items, {"name" : addItem}]);
-  }
+    setItems(items => [...items, {"name" : addItem, "completed" : false}]);
+  };
 
-  const deleteEntry = () => {
+  const deleteEntry = (index) => {
     // when X is pressed, will delete entry from list
-  }
+    let tempItems = items;
+    tempItems.splice(index, 1);
+    setItems(tempItems => [...tempItems]);
+  };
 
-  const markEntryComplete = (isComplete) => {
+  const markEntryComplete = (index) => {
     // Clicking the checkbox will strike through the text to mark the entry "complete"
-    setItems([...items, {"completed" : isComplete}]);
-  }
-  
-  console.log(items);
+    if (items[index] !== undefined) setItems(items => [...items, items[index].isComplete = !items[index].isComplete]);
+    
+  };
 
+  const updateFilter = (filter) => {
+    setFilter(filter);
+  };
+
+  let tempItems = items.length > 0 ? 
+    items.filter(item => {
+      if (filter === null) return item;
+      return item.isComplete === filter;                            
+    })
+    :
+    [];
+  
   return (
     <div className="App">
       <p>TODO</p>
       <AddEntry inputEntry={inputEntry}/>
-      <ToDoList items={items} deleteEntry={deleteEntry} markEntryComplete={markEntryComplete} />
+      <ToDoList
+        items={tempItems}
+        deleteEntry={deleteEntry}
+        markEntryComplete={markEntryComplete}
+        updateFilter={updateFilter}
+      />
     </div>
   );
 }
--- src/src/ToDoList.jsx
+++ src/src/ToDoList.jsx
@@ -2,12 +2,34 @@ import React from "react";
 
 const ToDoList = (props) => {
     return (
-        <div>
-            <ul>
-                {props.items.map((item, index) => (
-                    <li key={index}>{item.name}</li>
-                ))}
-            </ul>
+        <div>{
+            props.items.length <= 0 ?
+            <p>You currently don't have anything in your To Do List.</p>
+            :
+            <div className="container">
+                <div className="listConainer">
+                    <ul>
+                        {props.items.map((item, index) => (
+                            <li key={index}>
+                                <input type="checkbox"></input>
+                                <span>{item.name}</span>
+                                <div tabIndex="0" role="button" onClick={props.markEntryComplete()}>X</div>
+                            </li>
+                        ))}
+                    </ul>
+                    <div id="">
+                        <span>{props.items.length} items left</span>
+                        <div tabIndex="0" role="button">Clear Completed</div>
+                    </div>
+                </div>
+
+                <div className="filters">
+                    <div tabIndex="0" role="button" onClick={props.updateFilter(null)}>All</div>
+                    <div tabIndex="0" role="button" onClick={props.updateFilter(true)}>Active</div>
+                    <div tabIndex="0" role="button" onClick={props.updateFilter(false)}>Completed</div>
+                </div>
+            </div>
+        }
         </div>
     )
 }
