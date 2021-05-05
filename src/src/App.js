4d265a8047d7532a5d6a144ea3aece82f8df5911
AnthonyMarc23 <anthony@anthonymarc.me>
2021-05-05 11:47:51 -0400

    Filters Almost working, add, delete entries working

--- src/src/App.js
+++ src/src/App.js
@@ -1,49 +1,66 @@
 import './App.css';
 import AddEntry from './AddEntry.jsx';
 import ToDoList from './ToDoList.jsx';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const App = () => {
   const [items, setItems] = useState([]);
   const [filter, setFilter] = useState(null);
 
-  const inputEntry = (addItem) => {
+  const inputEntry = addItem => {
     //grab input from input field and save to items Array
     setItems(items => [...items, {"name" : addItem, "completed" : false}]);
   };
 
-  const deleteEntry = (index) => {
+  const deleteEntry = index => {
     // when X is pressed, will delete entry from list
     let tempItems = items;
     tempItems.splice(index, 1);
     setItems(tempItems => [...tempItems]);
+    console.log("Delete Single Entry")
   };
 
-  const markEntryComplete = (index) => {
+  const deleteCompleted = () => {
+    // This will clear all completed Entries
+    let tempItems = items;
+    tempItems.filter(item => item.completed === false);
+    setItems(tempItems => [...tempItems]);
+    console.log("Delete All Entries")
+  };
+
+  const markEntryComplete = index => {
     // Clicking the checkbox will strike through the text to mark the entry "complete"
     if (items[index] !== undefined) setItems(items => [...items, items[index].isComplete = !items[index].isComplete]);
-    
+    console.log(items[index]);
+    console.log("Entry marked complete")
   };
 
-  const updateFilter = (filter) => {
-    setFilter(filter);
-  };
+  const updateFilter = newFilter => {
+    if (newFilter === null) console.log("filter: Show All", filter);
+    if (newFilter === true) console.log("filter: Completed", filter);
+    if (newFilter === false) console.log("filter: Still to Do", filter);
+    setFilter(newFilter);
+  }
 
-  let tempItems = items.length > 0 ? 
+  const filteredItems = useEffect(() => {
+    const tempItems = items.length > 0 ? 
     items.filter(item => {
       if (filter === null) return item;
       return item.isComplete === filter;                            
     })
     :
     [];
-  
+    return tempItems
+  }, [items, filter]) || items;
+
   return (
     <div className="App">
       <p>TODO</p>
       <AddEntry inputEntry={inputEntry}/>
       <ToDoList
-        items={tempItems}
+        items={filteredItems}
         deleteEntry={deleteEntry}
+        deleteCompleted={deleteCompleted}
         markEntryComplete={markEntryComplete}
         updateFilter={updateFilter}
       />
--- src/src/ToDoList.jsx
+++ src/src/ToDoList.jsx
@@ -11,22 +11,22 @@ const ToDoList = (props) => {
                     <ul>
                         {props.items.map((item, index) => (
                             <li key={index}>
-                                <input type="checkbox"></input>
+                                <input type="checkbox" onSelect={props.markEntryComplete}></input>
                                 <span>{item.name}</span>
-                                <div tabIndex="0" role="button" onClick={props.markEntryComplete()}>X</div>
+                                <div tabIndex="0" role="button" onClick={props.deleteEntry}>X</div>
                             </li>
                         ))}
                     </ul>
                     <div id="">
                         <span>{props.items.length} items left</span>
-                        <div tabIndex="0" role="button">Clear Completed</div>
+                        <div tabIndex="0" role="button" onClick={props.deleteCompleted}>Clear Completed</div>
                     </div>
                 </div>
 
                 <div className="filters">
-                    <div tabIndex="0" role="button" onClick={props.updateFilter(null)}>All</div>
-                    <div tabIndex="0" role="button" onClick={props.updateFilter(true)}>Active</div>
-                    <div tabIndex="0" role="button" onClick={props.updateFilter(false)}>Completed</div>
+                    <div tabIndex="0" role="button" onClick={() => props.updateFilter(null)}>All</div>
+                    <div tabIndex="0" role="button" onClick={() => props.updateFilter(true)}>Active</div>
+                    <div tabIndex="0" role="button" onClick={() => props.updateFilter(false)}>Completed</div>
                 </div>
             </div>
         }
