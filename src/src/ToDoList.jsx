316cf9a3986d52bb98133fec8b39de9a6ee3d6c7
AnthonyMarc23 <anthony@anthonymarc.me>
2021-05-07 01:44:51 -0400

    stylized text for mobile/desktop, svgs, added variables.scss,

--- src/src/ToDoList.jsx
+++ src/src/ToDoList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./ToDoList.scss";
+import icon_close from "./images/icon__close.svg";
 
 const ToDoList = (props) => {
     return (
@@ -15,22 +16,31 @@ const ToDoList = (props) => {
                     }).map((item, index) => (
                         <li className="list-item" key={index}>
                             <span className="completed-checkbox" onClick={() => props.markEntryComplete(index)}>
-                                <input type="checkbox" checked={item.completed}></input>
+                                <input type="checkbox" readOnly checked={item.completed}></input>
                                 <span></span>
                             </span>
-                            <span className={item.completed ? "strike-through" : ''}>{item.name}</span>
-                            <div tabIndex="0" role="button" onClick={() => props.deleteEntry(index)}>X</div>
+                            <span className={`item-name ${item.completed ? "strike-through" : ''}`}>{item.name}</span>
+                            <div tabIndex="0" role="button" onClick={() => props.deleteEntry(index)}>
+                                <svg className="icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z" fill="white" fillOpacity="0.2"/>
+                                </svg>
+                            </div>
                         </li>
                     ))}
                     <li className="list-item" id="">
-                        <span>{props.items.length} items left</span>
+                        <span className="item-count">{props.items.filter(item => !item.completed).length} items left</span>
+                        <div className={ props.items.length <= 0 ? "hidden" : "desktop-filters"}>
+                            <div className="action-button" tabIndex="0" role="button" onClick={() => props.updateFilter(null)}>All</div>
+                            <div className="action-button" tabIndex="0" role="button" onClick={() => props.updateFilter(false)}>Active</div>
+                            <div className="action-button" tabIndex="0" role="button" onClick={() => props.updateFilter(true)}>Completed</div>
+                        </div>
                         <div className="action-button clear" tabIndex="0" role="button" onClick={props.deleteCompleted}>Clear Completed</div>
                     </li>
                 </ul>
                 }
             </div>
 
-            <div className={ props.items.length <= 0 ? "hidden" : "filters"}>
+            <div className={ props.items.length <= 0 ? "hidden" : "mobile-filters"}>
                 <div className="action-button" tabIndex="0" role="button" onClick={() => props.updateFilter(null)}>All</div>
                 <div className="action-button" tabIndex="0" role="button" onClick={() => props.updateFilter(false)}>Active</div>
                 <div className="action-button" tabIndex="0" role="button" onClick={() => props.updateFilter(true)}>Completed</div>
